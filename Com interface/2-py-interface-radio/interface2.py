c8837ba26213a9de83c509b66adfe83db6a8e998
Isabella010101 <isabella.n.oliveira@aluno.senai.br>
2025-08-12 11:47:09 -0300

    Extração dos arquivos
    Extração dos arquivos dos exercícios

--- Com interface/2-py-interface-radio/interface2.py
+++ Com interface/2-py-interface-radio/interface2.py
@@ -18,7 +18,7 @@ def atualizar_resultado():
         saudacao = "Bem-vindo"
 
     if var_check_personalizada.get():
-        saudacao += f"{saudacao}, caro(a)!"
+        saudacao += ", caro(a)!"
 
     # obtera cor fav selecionada 
     cor_favorita = combo_cor.get()
@@ -46,67 +46,87 @@ def limpar_campos():
 # criar a janela principal
 janela = tk.Tk()
 janela.title("Exemplo de Interface")
-janela.geometry("400x510")
-# janela.config(bg='red') # cor de fundo da janela
+janela.geometry("400x550")
+janela.config(bg='blue') # cor de fundo da janela
 
 
 # ================================================================================
 # criar uma caixa de entrada (entry), equivalente ao input do html
 # label é o textinho de cima e a caixa de texto é o entry
-label_nome = tk.Label(janela, text="Digite seu nome: ")
+label_nome = tk.Label(janela, text="Digite seu nome: ", bg='blue', fg='white', font=("Arial", 15, 'bold'))
 label_nome.pack(pady=5)
-caixa_texto = tk.Entry(janela, width=40)
+caixa_texto = tk.Entry(janela, width=20, bg="black", fg="white", font=("Arial", 20, 'bold'))
 caixa_texto.pack(pady=5)
 
 # =================================================================================
+# ----------------- Configuração dos Estilos -----------------
+style = ttk.Style()
+# Configura o estilo para os botões de rádio
+style.configure('Custom.TRadiobutton', background='blue', foreground='white', 
+                selectcolor='black', indicatorcolor='black', font=("Arial", 12, 'bold'))
+# Configura o estilo para os botões de rádio quando o mouse está sobre eles
+style.map('Custom.TRadiobutton',
+          background=[('active', 'blue')],
+          foreground=[('active', 'white')])
+
+# =================================================================================
 # criar botões de rádio para selecionar a preferência, apenas uma opção
-label_preferencia = tk.Label(janela, text="Escolha a sua preferência: ")
+label_preferencia = tk.Label(janela, text="Escolha a sua preferência: ", bg='blue', fg='white', font=("Arial", 15, 'bold'))
 label_preferencia.pack(pady=5)
 
 # radio - escolha para selecionar um só
 var_radio = tk.StringVar(value="Café") 
-radio_cafe = tk.Radiobutton(janela, text="Café", variable=var_radio, value="café")
-radio_cha = tk.Radiobutton(janela, text="Chá", variable=var_radio, value="chá")
-radio_suco = tk.Radiobutton(janela, text="Suco", variable=var_radio, value="suco")
-radio_agua = tk.Radiobutton(janela, text="Água", variable=var_radio, value="água")
 
-# .pack (método do tkinter para tornar os widgets visíveis)
+radio_cafe = ttk.Radiobutton(janela, text="Café", variable=var_radio, value="Café", style='Custom.TRadiobutton')
+radio_cha = ttk.Radiobutton(janela, text="Chá", variable=var_radio, value="Chá", style='Custom.TRadiobutton')
+radio_suco = ttk.Radiobutton(janela, text="Suco", variable=var_radio, value="Suco", style='Custom.TRadiobutton')
+radio_agua = ttk.Radiobutton(janela, text="Água", variable=var_radio, value="Água", style='Custom.TRadiobutton')
+
+# para deixar visível
 radio_cafe.pack()
 radio_cha.pack()
 radio_suco.pack()
 radio_agua.pack()
 
+
+
 # ================================================================================
 # criar caixas de seleção (checkbox) - aceita mais de uma seleção
+label_saudacao = tk.Label(janela, text="Escolha uma: ", bg='blue', fg='white', font=("Arial", 15, 'bold'))
+label_saudacao.pack(pady=5)
+
 var_check_saudacao = tk.BooleanVar()
-check_saudacao = tk.Checkbutton(janela, text="Usar saudação informal", variable=var_check_saudacao)
+check_saudacao = tk.Checkbutton(janela, text="Usar saudação informal", variable=var_check_saudacao, bg='blue', fg='black', font=("Arial", 12, 'bold'))
 check_saudacao.pack(pady=5)
 
 var_check_personalizada = tk.BooleanVar()
-check_personalizada = tk.Checkbutton(janela, text="Usar saudação personalizada", variable=var_check_personalizada)
+check_personalizada = tk.Checkbutton(janela, text="Usar saudação personalizada", variable=var_check_personalizada, bg='blue', fg='black', font=("Arial", 12, 'bold'))
 check_personalizada.pack(pady=5)
 
 # ================================================================================
 # combobox (caixa de seleção com opções)
-label_cor = tk.Label(janela, text="Selecione sua cor favorita: ")
+label_cor = tk.Label(janela, text="Selecione sua cor favorita: ", bg='blue', fg='white', font=("Arial", 15, 'bold'))
 label_cor.pack(pady=5)
 
 combo_cor = ttk.Combobox(janela, values=["Vermelho", "Azul", "Verde", "Amarelo", "Preto", "Branco"])
 combo_cor.set('Escolha uma cor')
 combo_cor.pack(pady=5)
 
+combo_cor.config(font=("Arial", 12, 'bold'), width=15)
+
 # =================================================================================
 # criar botões (Frase e limpar)
-botao_atualizar = tk.Button(janela, text="Atualizar", command=atualizar_resultado)
+botao_atualizar = tk.Button(janela, text="Atualizar", command=atualizar_resultado, bg='blue', fg='black', font=("Arial", 12, 'bold'))
 botao_atualizar.pack(pady=10)
 
-botao_limpar = tk.Button(janela, text="Limpar", command=limpar_campos)
+botao_limpar = tk.Button(janela, text="Limpar", command=limpar_campos, bg='orange', fg='black', font=("Arial", 12, 'bold'))
 botao_limpar.pack(pady=10)
 
 # =================================================================================
 # exibição do resultado final (rótulo "label")
 label_resultado = tk.Label(janela, text="", wraplength=350)
 label_resultado.pack(pady=10)
+
 
 # =================================================================================
 # executar a janela
